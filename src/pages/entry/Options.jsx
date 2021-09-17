f17d3016ac0fdfa41e15bb427f895bed25defbe2
levi-sr93 <levi-sr@outlook.com>
2021-09-17 18:26:50 -0300

    testing images | image alt testing | making the page and component for scoops

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+
+import ScoopOptions from './ScoopOptions';
+
+export default function Options({optionType}){
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3030/${optionType}`)
+      .then(response => setItems(response.data) )
+      .catch(error => {
+        //TODO: handle error response
+      })
+  }, [optionType])
+
+  //replace with ToppingOptions when available
+  const ItemComponent = optionType === 'scoops' ? ScoopOptions : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+       key={item.name}
+       name={item.name}
+       imagePath={item.imagePath}
+     />
+  ))
+
+  return <Row>{optionItems}</Row>
+}
